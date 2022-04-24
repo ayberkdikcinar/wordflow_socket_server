d8abba49c0fd328071d0410bbf96aa124ebf53b8
ayberkdikcinar <ayberkdikcinar@gmail.com>
2022-04-24 16:57:54 +0300

    port changed.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const io = require('socket.io')(server);
 
 const PORT = 3000;
 
-server.listen(PORT);
+server.listen(process.env.PORT || PORT);
 console.log(`Listening on port ${PORT}...`);
 
 let room;
